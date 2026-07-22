1fa6c8fd22558ce87392bae5286104e4b02e3a4f
Victor Brandão <victorbrandaotech@gmail.com>
2026-07-22 17:28:49 -0300

    cloudboost 4.3.2

--- Casks/cloudboost.rb
+++ Casks/cloudboost.rb
@@ -1,6 +1,6 @@
 cask "cloudboost" do
-  version "4.3.1"
-  sha256 "f2977cefed78874b17f48266f2cd57529e6c39270374f3b05d7ab347cacfc41f"
+  version "4.3.2"
+  sha256 "7e712cc65cacad898630536b44c0411bb5fa8fdffe25c77fe520f3a79933fe44"
 
   url "https://github.com/victorbrandaao/CloudBoost/releases/download/v#{version}/CloudBoost_v#{version}.dmg",
       verified: "github.com/victorbrandaao/CloudBoost/"
@@ -9,8 +9,6 @@ cask "cloudboost" do
   homepage "https://victorbrandaao.github.io/CloudBoost/"
 
   depends_on macos: :monterey
-  depends_on arch: :arm64
-
   app "CloudBoost.app"
 
   zap trash: [
